b2e6edae565589a43e671e15e6d6083611187bda
remcodk <remcodekoeijer@hotmail.com>
2017-03-28 14:54:08 +0200

    things..

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,14 @@
+#include <opencv2/opencv.hpp>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <iostream>
+#include <vector>
+using namespace std;
+using namespace cv;
+
+
+Mat get_hogdescriptor_visu(const Mat& color_origImg, vector<float>& descriptorValues, const Size & size);
 
 int main()
 {
@@ -21,10 +30,195 @@ int main()
 	Done, i guess
 	
 	*/
+	Size hogWinStride = Size(8, 8);
+	Size hogPadding = Size(0, 0);
 
-	cv::Mat img = cv::imread("data/pug_170.jpg");
-	cv::namedWindow("OpenCV");
-	cv::imshow("OpenCV", img);
-	cv::waitKey(0);
+	Mat img = cv::imread("data/pug_166.jpg", IMREAD_GRAYSCALE);
+	namedWindow("OpenCV");
+	imshow("OpenCV", img);
+
+	vector<float> descriptors;
+	vector<Point> locations;
+
+	HOGDescriptor hog;
+	hog.winSize = Size(128, 128); //size of the window to get the hog? so bounding box or entire image?
+
+	//locations is empty during training, because you don't find the location of the object you want to detect, you only extract features.
+	hog.compute(img, descriptors, hogWinStride, hogPadding, locations); 
+	//the descriptors here should be sufficient for training. 
+	//useful link https://github.com/DaHoC/trainHOG and also check the train_HOG.cpp in the opencv examples.
+
+	cout << hog.winSize.width << endl;
+	cout << hog.winSize.height << endl;
+	cout << "descriptors list size: " << descriptors.size() << endl;
+
+	imshow("gradient", get_hogdescriptor_visu(img.clone(), descriptors, Size(128, 128)));
+
+	//cout << "descriptors: " << descriptors[0] << endl;
+	cout << "locations: " << locations << endl;
+
+
+
+	waitKey(0);
+
 	return 0;
 }
+
+//From the train_hog.cpp, for visualizing a hogdescriptor. 
+Mat get_hogdescriptor_visu(const Mat& color_origImg, vector<float>& descriptorValues, const Size & size)
+{
+	const int DIMX = size.width;
+	const int DIMY = size.height;
+	float zoomFac = 3;
+	Mat visu;
+	resize(color_origImg, visu, Size((int)(color_origImg.cols*zoomFac), (int)(color_origImg.rows*zoomFac)));
+
+	int cellSize = 8;
+	int gradientBinSize = 9;
+	float radRangeForOneBin = (float)(CV_PI / (float)gradientBinSize); // dividing 180 into 9 bins, how large (in rad) is one bin?
+
+																	   // prepare data structure: 9 orientation / gradient strenghts for each cell
+	int cells_in_x_dir = DIMX / cellSize;
+	int cells_in_y_dir = DIMY / cellSize;
+	float*** gradientStrengths = new float**[cells_in_y_dir];
+	int** cellUpdateCounter = new int*[cells_in_y_dir];
+	for (int y = 0; y<cells_in_y_dir; y++)
+	{
+		gradientStrengths[y] = new float*[cells_in_x_dir];
+		cellUpdateCounter[y] = new int[cells_in_x_dir];
+		for (int x = 0; x<cells_in_x_dir; x++)
+		{
+			gradientStrengths[y][x] = new float[gradientBinSize];
+			cellUpdateCounter[y][x] = 0;
+
+			for (int bin = 0; bin<gradientBinSize; bin++)
+				gradientStrengths[y][x][bin] = 0.0;
+		}
+	}
+
+	// nr of blocks = nr of cells - 1
+	// since there is a new block on each cell (overlapping blocks!) but the last one
+	int blocks_in_x_dir = cells_in_x_dir - 1;
+	int blocks_in_y_dir = cells_in_y_dir - 1;
+
+	// compute gradient strengths per cell
+	int descriptorDataIdx = 0;
+	int cellx = 0;
+	int celly = 0;
+
+	for (int blockx = 0; blockx<blocks_in_x_dir; blockx++)
+	{
+		for (int blocky = 0; blocky<blocks_in_y_dir; blocky++)
+		{
+			// 4 cells per block ...
+			for (int cellNr = 0; cellNr<4; cellNr++)
+			{
+				// compute corresponding cell nr
+				cellx = blockx;
+				celly = blocky;
+				if (cellNr == 1) celly++;
+				if (cellNr == 2) cellx++;
+				if (cellNr == 3)
+				{
+					cellx++;
+					celly++;
+				}
+
+				for (int bin = 0; bin<gradientBinSize; bin++)
+				{
+					float gradientStrength = descriptorValues[descriptorDataIdx];
+					descriptorDataIdx++;
+
+					gradientStrengths[celly][cellx][bin] += gradientStrength;
+
+				} // for (all bins)
+
+
+				  // note: overlapping blocks lead to multiple updates of this sum!
+				  // we therefore keep track how often a cell was updated,
+				  // to compute average gradient strengths
+				cellUpdateCounter[celly][cellx]++;
+
+			} // for (all cells)
+
+
+		} // for (all block x pos)
+	} // for (all block y pos)
+
+
+	  // compute average gradient strengths
+	for (celly = 0; celly<cells_in_y_dir; celly++)
+	{
+		for (cellx = 0; cellx<cells_in_x_dir; cellx++)
+		{
+
+			float NrUpdatesForThisCell = (float)cellUpdateCounter[celly][cellx];
+
+			// compute average gradient strenghts for each gradient bin direction
+			for (int bin = 0; bin<gradientBinSize; bin++)
+			{
+				gradientStrengths[celly][cellx][bin] /= NrUpdatesForThisCell;
+			}
+		}
+	}
+
+	// draw cells
+	for (celly = 0; celly<cells_in_y_dir; celly++)
+	{
+		for (cellx = 0; cellx<cells_in_x_dir; cellx++)
+		{
+			int drawX = cellx * cellSize;
+			int drawY = celly * cellSize;
+
+			int mx = drawX + cellSize / 2;
+			int my = drawY + cellSize / 2;
+
+			rectangle(visu, Point((int)(drawX*zoomFac), (int)(drawY*zoomFac)), Point((int)((drawX + cellSize)*zoomFac), (int)((drawY + cellSize)*zoomFac)), Scalar(100, 100, 100), 1);
+
+			// draw in each cell all 9 gradient strengths
+			for (int bin = 0; bin<gradientBinSize; bin++)
+			{
+				float currentGradStrength = gradientStrengths[celly][cellx][bin];
+
+				// no line to draw?
+				if (currentGradStrength == 0)
+					continue;
+
+				float currRad = bin * radRangeForOneBin + radRangeForOneBin / 2;
+
+				float dirVecX = cos(currRad);
+				float dirVecY = sin(currRad);
+				float maxVecLen = (float)(cellSize / 2.f);
+				float scale = 2.5; // just a visualization scale, to see the lines better
+
+								   // compute line coordinates
+				float x1 = mx - dirVecX * currentGradStrength * maxVecLen * scale;
+				float y1 = my - dirVecY * currentGradStrength * maxVecLen * scale;
+				float x2 = mx + dirVecX * currentGradStrength * maxVecLen * scale;
+				float y2 = my + dirVecY * currentGradStrength * maxVecLen * scale;
+
+				// draw gradient visualization
+				line(visu, Point((int)(x1*zoomFac), (int)(y1*zoomFac)), Point((int)(x2*zoomFac), (int)(y2*zoomFac)), Scalar(0, 255, 0), 1);
+
+			} // for (all bins)
+
+		} // for (cellx)
+	} // for (celly)
+
+
+	  // don't forget to free memory allocated by helper data structures!
+	for (int y = 0; y<cells_in_y_dir; y++)
+	{
+		for (int x = 0; x<cells_in_x_dir; x++)
+		{
+			delete[] gradientStrengths[y][x];
+		}
+		delete[] gradientStrengths[y];
+		delete[] cellUpdateCounter[y];
+	}
+	delete[] gradientStrengths;
+	delete[] cellUpdateCounter;
+
+	return visu;
+
+} // get_hogdescriptor_visu
